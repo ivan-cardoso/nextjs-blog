9d342f7a8d10439a23356ed2b6396792ee1549c0
Ivan Cardoso <ivancardoso59@gmail.com>
2025-05-14 14:02:07 -0300

    Footer desktop

--- app/ui/footer/footer.tsx
+++ app/ui/footer/footer.tsx
@@ -1,18 +1,48 @@
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="border-t mt-16">
-      <div className="max-w-5xl mx-auto py-8 px-4 flex flex-col md:flex-row items-center justify-between text-sm text-muted-foreground">
-        <p>© {new Date().getFullYear()} YourBlogName. All rights reserved.</p>
-        <div className="flex space-x-4 mt-4 md:mt-0">
-          <Link href="/" className="hover:underline">
-            Home
-          </Link>
-          <Link href="/about" className="hover:underline">
-            About
-          </Link>
-          {/* Future: maybe GitHub, Twitter links */}
+    <footer className="mt-16 h-32 px-6 md:px-10">
+      <div className="border-t py-8 h-full flex flex-col md:flex-row items-center justify-between text-sm 2xl:text-base 3xl:text-lg text-muted-foreground font-geist">
+        <p className="uppercase">
+          Made in Argentina — All rights reserved, Ivan Cardoso{" "}
+          {new Date().getFullYear()}
+        </p>
+        <div className="space-x-4 mt-4 md:mt-0">
+          <div className="flex items-center space-x-5 ml-auto">
+            <Link
+              href="https://linkedin.com/in/ivan--cardoso"
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                "text-primary-text text-sm 2xl:text-base 3xl:text-lg  hover:text-highlight uppercase"
+              )}
+            >
+              LinkedIn
+            </Link>
+            <Link
+              href="https://ivancardoso.vercel.app"
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                "text-primary-text text-sm 2xl:text-base 3xl:text-lg  hover:text-highlight uppercase"
+              )}
+            >
+              Portfolio
+            </Link>
+
+            <Link
+              href="https://github.com/ivan-cardoso"
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                "text-primary-text text-sm 2xl:text-base 3xl:text-lg  hover:text-highlight uppercase"
+              )}
+            >
+              GitHub
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
